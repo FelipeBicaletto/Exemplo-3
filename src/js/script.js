221e88719d1291df0c79f30a3c2b5715c0ec81ed
Felipe <febicaletto@gmail.com>
2025-03-31 09:08:31 -0300

    Declarações e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+//DECLARAÇÕES E VARIAVEIS
+
+var nome = "FIAP";
+console.log(nome)
+
+let idade = 19;
+console.log(idade)
+
+const sobrenome = "Fiapinho";
+console.log(sobrenome)
+
+//DECLARANDO UMA VARIAVEL INDEFINIDA    
+let nome1;
+console.log(nome1)
+
+//DECLARANDO UMA VARIAVEL NULA
+let nome2 = null;
+console.log(nome2)
