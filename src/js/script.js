ea712f0998f091b87ff3ce5d50220f5e672880d9
Felipe <febicaletto@gmail.com>
2025-03-31 09:54:10 -0300

    Operadores aritimeticos, lógicos e de comparação

--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,79 @@
-//DECLARAÇÕES E VARIAVEIS
+// //DECLARAÇÕES E VARIAVEIS
 
-var nome = "FIAP";
-console.log(nome)
+// var nome = "FIAP";
+// console.log(nome)
 
-let idade = 19;
-console.log(idade)
+// let idade = 19;
+// console.log(idade)
 
-const sobrenome = "Fiapinho";
-console.log(sobrenome)
+// const sobrenome = "Fiapinho";
+// console.log(sobrenome)
 
-//DECLARANDO UMA VARIAVEL INDEFINIDA    
-let nome1;
-console.log(nome1)
+// //DECLARANDO UMA VARIAVEL INDEFINIDA    
+// let nome1;
+// console.log(nome1)
 
-//DECLARANDO UMA VARIAVEL NULA
-let nome2 = null;
-console.log(nome2)
+// //DECLARANDO UMA VARIAVEL NULA
+// let nome2 = null;
+// console.log(nome2)
 
-//TIPOS DE VARIAVEIS
-let exemplo1 = 10;
-console.log(typeof exemplo1)
+// //TIPOS DE VARIAVEIS
+// let exemplo1 = 10;
+// console.log(typeof exemplo1)
 
-let exemplo2 = "Tecnologia";
-console.log(typeof exemplo2)
+// let exemplo2 = "Tecnologia";
+// console.log(typeof exemplo2)
 
-let exemplo3 = true;
-console.log(typeof exemplo3)
+// let exemplo3 = true;
+// console.log(typeof exemplo3)
 
-let exemplo4 = {};
-console.log(typeof exemplo4)
+// let exemplo4 = {};
+// console.log(typeof exemplo4)
 
-let exemplo5 = [];
-console.log(typeof exemplo5)
+// let exemplo5 = [];
+// console.log(typeof exemplo5)
 
-//CONVERSÕES
-let numFloat = 123.456;
-console.log(parseInt(numFloat))
+// //CONVERSÕES
+// let numFloat = 123.456;
+// console.log(parseInt(numFloat))
 
-let numString = "123.456";
-console.log(parseFloat(numString))
+// let numString = "123.456";
+// console.log(parseFloat(numString))
 
-//METODOS
+// //METODOS
 
-//length = verifica o tamanho da string
-let frase = "O mundo da tecnologia";
-console.log(frase.length)
+// //length = verifica o tamanho da string
+// let frase = "O mundo da tecnologia";
+// console.log(frase.length)
 
-//indexOf = retorna um trecho do texto
-let texto = "A programação indomável e sustentável";
-console.log(texto.indexOf("ão"))
+// //indexOf = retorna um trecho do texto
+// let texto = "A programação indomável e sustentável";
+// console.log(texto.indexOf("ão"))
 
-//slice = retorna um trecho do texto com inicio e final 
-let info = "Processamento de ponta";
-console.log(info.slice(2,6))
+// //slice = retorna um trecho do texto com inicio e final 
+// let info = "Processamento de ponta";
+// console.log(info.slice(2,6))
+
+//OPERADORES ARITIMÉTICOS
+
+// const num1 = 10;
+// const num2 = 20;
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+
+//OPERADORES LÓGICOS
+
+const num3 = 10;
+const num4 = 20;
+
+console.log(num3 < num4)
+console.log(num3 > num4 && num4 < num3)
+console.log(num3 < num4 || num4 < num3)
+
+//OPERADORES DE COMPARAÇÃO
+
+console.log(num3 == num4)
+console.log(num3 === num4)
